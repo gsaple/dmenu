9fcdffd5e2da30b8b9ba6a87c4b9d5c858f59d84
gsaple <yymelau@gmail.com>
2022-08-23 14:24:45 +1000

    xresources patched

--- config.def.h
+++ config.def.h
@@ -10,15 +10,25 @@ static const char *fonts[] = {
         "JoyPixels:pixelsize=16:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+static char normfgcolor[] =   "#bbbbbb";
+static char normbgcolor[] =   "#222222";
+static char selfgcolor[]  =   "#eeeeee";
+static char selbgcolor[]  =   "#005577";
+static char hlnormfgcolor[] = "#ffc978";
+static char hlnormbgcolor[] = "#222222";
+static char hlselfgcolor[]  = "#ffc978";
+static char hlselbgcolor[]  = "#005577";
+static char *colors[SchemeLast][2] = {
+	                        /*     fg         bg      */
+	[SchemeNorm] =          { normfgcolor,   normbgcolor },
+	[SchemeSel] =           { selfgcolor,    selbgcolor},
+	[SchemeNormHighlight] = { hlnormfgcolor, hlnormbgcolor},
+	[SchemeSelHighlight] =  { hlselfgcolor,  hlselbgcolor},
+	[SchemeOut] =           { "#000000",     "#00ffff" },
+	[SchemeOutHighlight] =  { "#ffc978",     "#00ffff" },
 };
+
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
@@ -30,3 +40,17 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static const unsigned int border_width = 3;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "color7",     STRING, &normfgcolor  },
+	{ "background", STRING, &normbgcolor  },
+	{ "color15",    STRING, &selfgcolor   },
+	{ "color4",     STRING, &selbgcolor   },
+	{ "color11",    STRING, &hlnormfgcolor},
+	{ "background", STRING,	&hlnormbgcolor},
+	{ "color11",    STRING, &hlselfgcolor },
+	{ "color4",     STRING, &hlselbgcolor },
+};
